b7857654199f59c9893db70da70ff2663f2d4514
gbeatrix <gatibeatrix+github@gmail.com>
2021-11-29 16:27:27 +0100

    day01 NumberSequence test random update

--- week08-lab-tasks/src/test/java/day01/NumberSequenceTest.java
+++ week08-lab-tasks/src/test/java/day01/NumberSequenceTest.java
@@ -16,7 +16,6 @@ class NumberSequenceTest {
             numbers.add(i);
         }
         NumberSequence ns1 = new NumberSequence(numbers);
-        assertEquals(10, ns1.getNumbers().size());
         assertEquals(numbers, ns1.closeToAverage(5));
 
         numbers.remove(9);
@@ -24,23 +23,33 @@ class NumberSequenceTest {
         assertEquals(numbers, ns1.closeToAverage(4));
 
         assertEquals(Arrays.asList(4, 5, 6, 7), ns1.closeToAverage(2));
+        assertEquals(10, ns1.getNumbers().size());
     }
 
     @Test
     void testRandom() {
-        List<Integer> numbers = new NumberSequence(100, 1, 10).getNumbers();
+        List<Integer> numbers = new NumberSequence(100, 15, 25).getNumbers();
         assertEquals(100, numbers.size());
 
         int minValue = numbers.get(0);
         for (int number : numbers) {
             minValue = Math.min(number, minValue);
         }
-        assertTrue(1 <= minValue);
+        assertTrue(15 <= minValue);
 
         int maxValue = numbers.get(0);
         for (int number : numbers) {
             maxValue = Math.max(number, maxValue);
         }
-        assertTrue(10 >= maxValue);
+        assertTrue(25 >= maxValue);
+
+        List<Integer> distincts = new ArrayList<>();
+        for (int number : numbers) {
+            if (!distincts.contains(number)) {
+                distincts.add(number);
+            }
+        }
+        assertTrue(8 <= distincts.size());
+        assertTrue(distincts.size() <= 11);
     }
 }
